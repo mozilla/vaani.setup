c1a5a1430e64146e547aa82ab9dbd347109080b3
David Flanagan <dflanagan@mozilla.com>
2016-07-20 15:42:09 -0700

    aplay needs a -D option on the Edison

--- platforms/EdisonYocto.js
+++ platforms/EdisonYocto.js
@@ -2,6 +2,9 @@ module.exports = {
   platform: 'edison',
   // On our edison devices, this is how we talk to the microphone
   microphoneDevice: 'plughw:2,0', 
+
+  playAudio: 'aplay -q -D plughw:3,0 $AUDIO',
+
   // The edison is configured so that starting hostapd automatically
   // starts udhcpd and does the ifconfig as well
   startAP: 'systemctl start hostapd',
